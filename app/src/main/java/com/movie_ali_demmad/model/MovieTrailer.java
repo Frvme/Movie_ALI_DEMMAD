fc1658e5e8192539a8da7f85efb70c77c3cf5108
Anas <anasdemmad@gmail.com>
2019-01-11 11:50:46 +0100

    Second commit

---
+++ app/src/main/java/com/movie_ali_demmad/model/MovieTrailer.java
@@ -0,0 +1,4 @@
+package com.movie_ali_demmad.model;
+
+public class MovieTrailer {
+}
--- app/src/main/java/com/movie_ali_demmad/ui/MainActivity.java
+++ app/src/main/java/com/movie_ali_demmad/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.movie_ali_demmad;
+package com.movie_ali_demmad.ui;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.movie_ali_demmad.R;
 
 public class MainActivity extends AppCompatActivity {
 
